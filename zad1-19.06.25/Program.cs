bcab3e1c0dbe9a11b8ea3e35306b331fb70f3a61
Gergana Ivanova <gslavcheva933@gmail.com>
2025-06-19 10:38:40 +0300

    Add files via upload

---
+++ zad1-19.06.25/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1_19._06._25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> lettersCounts = new Dictionary<string, int>();
+            string name;
+            while ((name = Console.ReadLine()) != "stop")
+            {
+                if (lettersCounts.ContainsKey(name))
+                {
+                    lettersCounts[name]++;
+                }
+                else 
+                {
+                    lettersCounts.Add(name, 1);
+                }
+            }
+            foreach (var entry in lettersCounts) 
+            {
+                Console.WriteLine($"{entry.Key} -> {entry.Value} ");
+            }
+        }
+    }
+}
---
+++ zad2- 19.06.25/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2__19._06._25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> studentBooks = new Dictionary<string,List<string>>();
+            List<string> studentOrder = new List<string>();
+            string line;
+            while ((line = Console.ReadLine()) != "end")
+            {
+                string[] parts = line.Split(new string[] {" -> "}, StringSplitOptions.None);
+                string studentName = parts[0];
+                string bookTitle = parts[1];
+
+                if (!studentBooks.ContainsKey(studentName))
+                {
+                    studentBooks.Add(studentName, new List<string>());
+                    studentOrder.Add(studentName);
+                }
+                studentBooks[studentName].Add(bookTitle);
+            }
+            foreach (string studentName in studentOrder) 
+            {
+                Console.WriteLine($"{studentName}: {string.Join(", ", studentBooks[studentName])}");
+            }
+        }
+    }
+}
